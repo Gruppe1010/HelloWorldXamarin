0e9c12acbf74ade454aa38e28fc1f043c2e568b9
RRoager <rasmus-roager@hotmail.com>
2021-10-27 09:19:52 +0200

    First commit

---
+++ ChristiansøApp/ChristiansøApp/ChristiansøApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ChristiansøApp.ViewModels;
+using ChristiansøApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ChristiansøApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ ChristiansøApp/ChristiansøApp/ChristiansøApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChristiansøApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChristiansøApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
