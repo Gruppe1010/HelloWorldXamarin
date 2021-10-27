31961c687d056e3225f375d494d064b402074424
RRoager <rasmus-roager@hotmail.com>
2021-10-27 09:50:18 +0200

    Vi proever igen

---
+++ ChristiansoeApp/ChristiansoeApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ChristiansoeApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ChristiansoeApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ ChristiansoeApp/ChristiansoeApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ChristiansoeApp.Models;
+using ChristiansoeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChristiansoeApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
